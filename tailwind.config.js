c6260e84e09ff780ec44dc24ca6bb49542b84b2f
Maruf <marufkhan2002@gmail.com>
2025-08-12 23:33:59 +0530

    Migrate CRA to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
